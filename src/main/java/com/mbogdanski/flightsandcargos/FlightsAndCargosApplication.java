9ad22c66a1984b19e8d05b549c60097fff5d3f59
Maciej Bogdanski <maciej.bogdanski@onegini.com>
2021-02-03 22:14:35 +0100

    spring boot initializr

---
+++ src/main/java/com/mbogdanski/flightsandcargos/FlightsAndCargosApplication.java
@@ -0,0 +1,13 @@
+package com.mbogdanski.flightsandcargos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsAndCargosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsAndCargosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbogdanski/flightsandcargos/FlightsAndCargosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbogdanski.flightsandcargos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsAndCargosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
